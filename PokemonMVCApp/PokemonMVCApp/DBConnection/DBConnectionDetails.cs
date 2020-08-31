de7a0ccf936c1c69470283e8158b2324834f447d
Matt McParland <matthew.mcparland@gmail.com>
2020-08-31 16:37:46 +0100

    Update DBConnectionDetails.cs

--- PokemonMVCApp/PokemonMVCApp/DBConnection/DBConnectionDetails.cs
+++ PokemonMVCApp/PokemonMVCApp/DBConnection/DBConnectionDetails.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 namespace PokemonMVCApp.DBConnection
 {
     public class DatabaseConnectionDetails
     {
         public static string DataSource = "matthewmcp-pokemon.database.windows.net";
         public static string UserID = "matthewmcp";
-        public static string Password = @"irlLahsh?n89t";
+        public static string Password = @"dummyPassword";
         public static string InitialCatalog = "PokemonV0.1";
     }
 }
